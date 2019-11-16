c7068d2a6429cb22a0a126cda08e39d6f30a2d0c
jiho <wlgh325@naver.com>
2019-11-17 03:49:04 +0900

    initialize 체육복 problem

---
+++ Level1/체육복/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Main{
+    public static void main(String[] args){
+        int n = 5;
+        int[] lost = {2,4};
+        int[] reserve = {1,3,5};
+
+        int n2 = 5;
+        int[] lost2 = {2,4};
+        int[] reserve2 = {3};
+
+        int answer = solution(n, lost, reserve);
+        System.out.println(answer);
+    }
+
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+        
+        return answer;
+    }
+}
---
+++ Level1/체육복/README.md
@@ -0,0 +1,11 @@
+# 프로그래머스(Programmers) 코딩테스트 연습
+## LEVEL 1 체육복 탐욕법(Greedy) 문제 자바(JAVA) 풀이
+- https://programmers.co.kr/learn/courses/30/lessons/42862?language=java
+
+## 티스토리 주소
+- http://hoho325.tistory.com/
+
+
+
+## 문제 풀이
+
