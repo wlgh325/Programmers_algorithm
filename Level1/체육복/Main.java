3757ec2c43597c577daaa109e757682bc5c1e5a4
jiho <wlgh325@naver.com>
2019-11-17 16:20:13 +0900

    solve the 체육복 problem

--- Level1/체육복/Main.java
+++ Level1/체육복/Main.java
@@ -1,10 +1,11 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Main{
     public static void main(String[] args){
-        int n = 5;
-        int[] lost = {2,4};
-        int[] reserve = {1,3,5};
+        int n = 5;  // 전체 학생
+        int[] lost = {2,4};     // 체육복을 도난당한 번호
+        int[] reserve = {1,3,5};    // 체육복 두 벌 있는 번호
 
         int n2 = 5;
         int[] lost2 = {2,4};
@@ -14,10 +15,45 @@ class Main{
         System.out.println(answer);
     }
 
-    public static int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+    // 앞 번호나 뒷 번호에만 빌려줄 수 있음
+    // 여벌 체육복이 있는 학생이 도난당했을 수도 있음 -> 이떄 여별이 있어도 빌려줄 수 없음
 
-        
+    
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int[] clothes = new int[n+2];
+        int answer = n;
+        clothes[0] = -10;
+        clothes[n+1] = -10;
+
+
+        for(int i=0; i<lost.length; i++){
+            clothes[lost[i]] -= 1;
+        }
+
+        for(int i=0; i<reserve.length; i++){
+            clothes[reserve[i]] += 1;
+        }
+
+        for(int i=0; i<=n; i++){
+            // 체육복이 없다면
+            if(clothes[i] == -1){
+                // 왼쪽 학생이 여벌 옷이 있다면 빌림
+                if(clothes[i-1] == 1){
+                    clothes[i-1] -= 1;
+                    clothes[i] += 1;
+                }
+                // 오른쪽 학생이 여별 옷이 있다면 빌림
+                else if(clothes[i+1] == 1){
+                    clothes[i+1] -= 1;
+                    clothes[i] += 1;
+                }
+                // 빌릴 수 없다면
+                else{
+                    answer --;
+                }
+            }
+        }
+
         return answer;
     }
 }
--- Level1/체육복/README.md
+++ Level1/체육복/README.md
@@ -1,6 +1,6 @@
 # 프로그래머스(Programmers) 코딩테스트 연습
 ## LEVEL 1 체육복 탐욕법(Greedy) 문제 자바(JAVA) 풀이
-- https://programmers.co.kr/learn/courses/30/lessons/42862?language=java
+- https://programmers.co.kr/learn/courses/30/lessons/42862
 
 ## 티스토리 주소
 - http://hoho325.tistory.com/
@@ -8,4 +8,9 @@
 
 
 ## 문제 풀이
-
+- 이 문제는 양 옆의 사람이 여벌의 옷을 들고 있는지 확인하여 빌림으로써 간단히 풀 수 있다.
+1. 우선 index를 쉽게 계산하기 위해서 맨 왼쪽과 맨 오른쪽에 칸을 추가한다.(clothes.length = n+2)
+2. lost를 순회하며 옷이 없는 학생은 -1을 해준다.
+3. reserve를 순회하며 옷이 더 있는 학생은 +1을 해준다.
+4. 그리고 clothes를 순회하며 옷이 없는 경우 즉 clothes[i]의 값이 -1인 경우 왼쪽과 오른쪽의 사람에게서 빌릴 수 있는지 검사한다.
+5. 체육복이 없는데 양쪽 사람도 모두 여벌 옷이 없는 경우 그 사람은 체육수업을 들을 수 없음으로 -1을 해준다.
